0306184f87debef6585780a420ec6e5200f84534
extense <kamaentertainmentstudio@gmail.com>
2018-06-08 21:28:02 +0300

    update 2

--- dsig/verify.go
+++ dsig/verify.go
@@ -1,11 +1,11 @@
 package dsig
 
 import (
+	"github.com/5teven/xmlsec"
+	"github.com/5teven/xmlsec/clib"
+	"github.com/5teven/xmlsec/crypto"
 	"github.com/lestrrat-go/libxml2/parser"
 	"github.com/lestrrat-go/libxml2/xpath"
-	"github.com/lestrrat-go/xmlsec"
-	"github.com/lestrrat-go/xmlsec/clib"
-	"github.com/lestrrat-go/xmlsec/crypto"
 )
 
 func NewSignatureVerify() (*SignatureVerify, error) {
